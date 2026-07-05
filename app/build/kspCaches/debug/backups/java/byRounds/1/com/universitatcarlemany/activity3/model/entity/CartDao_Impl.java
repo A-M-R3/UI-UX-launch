85fcb2626577c5981f27ee22456e4c0e71495fb8
A-M-R3 <amr.academics@gmail.com>
2026-07-05 18:21:43 +0200

    Implementacion de consumo de API con Retrofit y persistencia local con Room para Actividad 3

---
+++ app/build/kspCaches/debug/backups/java/byRounds/1/com/universitatcarlemany/activity3/model/entity/CartDao_Impl.java
@@ -0,0 +1,161 @@
+package com.universitatcarlemany.activity3.model.entity;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.annotation.NonNull;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.annotation.processing.Generated;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class CartDao_Impl implements CartDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<CartItem> __insertionAdapterOfCartItem;
+
+  private final SharedSQLiteStatement __preparedStmtOfClearCart;
+
+  public CartDao_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfCartItem = new EntityInsertionAdapter<CartItem>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR REPLACE INTO `cart_items` (`id`,`menuItemId`,`restaurantId`,`name`,`price`,`quantity`) VALUES (nullif(?, 0),?,?,?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final CartItem entity) {
+        statement.bindLong(1, entity.getId());
+        statement.bindString(2, entity.getMenuItemId());
+        statement.bindString(3, entity.getRestaurantId());
+        statement.bindString(4, entity.getName());
+        statement.bindDouble(5, entity.getPrice());
+        statement.bindLong(6, entity.getQuantity());
+      }
+    };
+    this.__preparedStmtOfClearCart = new SharedSQLiteStatement(__db) {
+      @Override
+      @NonNull
+      public String createQuery() {
+        final String _query = "DELETE FROM cart_items WHERE restaurantId = ?";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public Object insertCartItem(final CartItem item, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfCartItem.insert(item);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object clearCart(final String restId, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        final SupportSQLiteStatement _stmt = __preparedStmtOfClearCart.acquire();
+        int _argIndex = 1;
+        _stmt.bindString(_argIndex, restId);
+        try {
+          __db.beginTransaction();
+          try {
+            _stmt.executeUpdateDelete();
+            __db.setTransactionSuccessful();
+            return Unit.INSTANCE;
+          } finally {
+            __db.endTransaction();
+          }
+        } finally {
+          __preparedStmtOfClearCart.release(_stmt);
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object getCartForRestaurant(final String restId,
+      final Continuation<? super List<CartItem>> $completion) {
+    final String _sql = "SELECT * FROM cart_items WHERE restaurantId = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    _statement.bindString(_argIndex, restId);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<List<CartItem>>() {
+      @Override
+      @NonNull
+      public List<CartItem> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfMenuItemId = CursorUtil.getColumnIndexOrThrow(_cursor, "menuItemId");
+          final int _cursorIndexOfRestaurantId = CursorUtil.getColumnIndexOrThrow(_cursor, "restaurantId");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfPrice = CursorUtil.getColumnIndexOrThrow(_cursor, "price");
+          final int _cursorIndexOfQuantity = CursorUtil.getColumnIndexOrThrow(_cursor, "quantity");
+          final List<CartItem> _result = new ArrayList<CartItem>(_cursor.getCount());
+          while (_cursor.moveToNext()) {
+            final CartItem _item;
+            final int _tmpId;
+            _tmpId = _cursor.getInt(_cursorIndexOfId);
+            final String _tmpMenuItemId;
+            _tmpMenuItemId = _cursor.getString(_cursorIndexOfMenuItemId);
+            final String _tmpRestaurantId;
+            _tmpRestaurantId = _cursor.getString(_cursorIndexOfRestaurantId);
+            final String _tmpName;
+            _tmpName = _cursor.getString(_cursorIndexOfName);
+            final double _tmpPrice;
+            _tmpPrice = _cursor.getDouble(_cursorIndexOfPrice);
+            final int _tmpQuantity;
+            _tmpQuantity = _cursor.getInt(_cursorIndexOfQuantity);
+            _item = new CartItem(_tmpId,_tmpMenuItemId,_tmpRestaurantId,_tmpName,_tmpPrice,_tmpQuantity);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, $completion);
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
